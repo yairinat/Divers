0fa8706cc50b2c7c99b87dba19df96bb61797b38
yairinat <yair.rinat@mail.huji.ac.il>
2016-11-15 11:59:24 +0200

    function that reads the hydrographs

---
+++ diver_data.R
@@ -0,0 +1,10 @@
+
+  diver_data <- function (filename)
+  {
+    head(filename)
+    
+    x=read.csv(filename, header = TRUE)
+         #      , sep = ",", quote = "\"",
+      #       dec = ".", fill = TRUE, comment.char = "", ...)  
+    return(x) 
+  }
