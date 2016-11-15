8489615a8ebc4c0aa1c6e1cb34a746e2cd3961ab
yairinat <yair.rinat@mail.huji.ac.il>
2016-11-15 10:52:21 +0200

    first version

---
+++ main_script.R
@@ -0,0 +1,17 @@
+# main scipt,
+# 1) call a function that reads diver data:
+
+folder_path = "Q:/Yair/DataAnalysis/Divers/Data for Hydamass/Analyzed data/Soker"
+# list files in directory:\
+
+Soker_files=list.files(path = folder_path, pattern = NULL, all.files = FALSE,
+                        full.names = FALSE, recursive = FALSE,
+                        ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
+
+# loop on all the files in the directory and call the reading function
+
+  for (i in 1:12)
+  {
+  print(Soker_files[i])
+  
+}
